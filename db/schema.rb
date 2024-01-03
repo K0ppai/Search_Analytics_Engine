8d9ced40b04f3a5a5d639953344600583d7eb7dc
K0ppai <psthu.koppai@gmail.com>
2024-01-03 22:50:11 +0630

    Create routes and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_03_161240) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_03_161605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "query"
+    t.integer "count"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -27,4 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_03_161240) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
+  add_foreign_key "searches", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+john = User.create(
+  email: "john@gmail.com",
+  password: "123123",
+  name: "John",
+)
+
+tom = User.create(
+  email: "tom@gmail.com",
+  password: "123123",
+  name: "Tom",
+)
+
+10.times do
+  Article.create(
+    title: Faker::Name.unique.name,
+    content: Faker::Lorem.paragraph_by_chars,
+    user_id: john.id,
+  )
+end
+
+10.times do
+  Article.create(
+    title: Faker::Name.unique.name,
+    content: Faker::Lorem.paragraph_by_chars,
+    user_id: tom.id,
+  )
+end
