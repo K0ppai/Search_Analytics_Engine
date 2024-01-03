8862f9e6c015f071e85cc2e69a6d0f390d360656
K0ppai <psthu.koppai@gmail.com>
2024-01-04 03:25:16 +0630

    Add comments for code

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -2,9 +2,12 @@ module SearchesHelper
   def search_valid?(input)
     words = input.strip.split
 
+    # Questions must be at least 3 words long because of the structure of a question in English language
     return false unless words.length >= 3
 
-    words[-1].length > 2 && words[0].length >= 2
+    # Only save searches where the first word is at least 2 characters long for questions(Is it, Do you) and last word is at least 2 characters long so that questions doesn't end with single character
+
+    words[-1].length >= 2 && words[0].length >= 2
   end
 
   def update_or_create_search(query, user)
