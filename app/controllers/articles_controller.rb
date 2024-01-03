3a03daee4432ac44c0498fb0d7c4fa9e92d26b60
K0ppai <psthu.koppai@gmail.com>
2024-01-04 00:40:58 +0630

    Create helper functions for searches

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,78 +1,20 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  include SearchesHelper
+  before_action :set_query, only: %i[ index ]
 
-  # GET /articles or /articles.json
   def index
-    if params[:query].present?
-      @articles = Article.search(params[:query]).includes(:user)
-      # if is_sentence?(params[:query])
-      #   Search.create(user: current_user, query: params[:query])
-      # end
+    if @query.present?
+      @articles = Article.search(@query).includes(:user)
+
+      update_or_create_search(@query, current_user) if search_valid?(@query)
     else
       @articles = Article.all.includes(:user)
     end
   end
 
-  # GET /articles/1 or /articles/1.json
-  def show
-  end
-
-  # GET /articles/new
-  def new
-    @article = Article.new
-  end
-
-  # GET /articles/1/edit
-  def edit
-  end
-
-  # POST /articles or /articles.json
-  def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /articles/1 or /articles/1.json
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /articles/1 or /articles/1.json
-  def destroy
-    @article.destroy
-
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_article
-    @article = Article.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def article_params
-    params.require(:article).permit(:title, :content, :user_id)
+  def set_query
+    @query = params[:query]
   end
 end
--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -1,2 +1,22 @@
 module SearchesHelper
+  def search_valid?(input)
+    words = input.strip.split(" ")
+
+    if words.length >= 3
+      words.each do |word|
+        return false if word.length < 2
+      end
+      return true
+    end
+  end
+
+  def update_or_create_search(query, user)
+    existing_search = Search.find_by(query: query.strip, user_id: user.id)
+
+    if existing_search
+      existing_search.update(count: existing_search.count + 1)
+    else
+      Search.create(query:, user:)
+    end
+  end
 end
