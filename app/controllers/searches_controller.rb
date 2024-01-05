c304390481204c5c0a5c1fbfac1b9bf99a6f4b0a
K0ppai <psthu.koppai@gmail.com>
2024-01-05 19:04:20 +0630

    Update search controller for valid searches

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,19 @@
 class SearchesController < ApplicationController
+  include SearchesHelper
+
   def index
-    @searches = Search.all.includes(:user).where(user_id: current_user.id).order(count: :desc).limit(10)
+    @user_searches = current_user.searches.where(confirm: false)
+    @searches = Search.all.includes(:user).where(user_id: current_user.id, confirm: true).order(count: :desc).limit(10)
+
+    if @user_searches.present?
+      queries = convert_queries_to_string(@user_searches)
+
+      ids = filter_valid_queries_ids(queries)
+
+      @user_searches.where(id: ids).update_all(confirm: true) if ids.any?
+      @user_searches.where.not(id: ids).delete_all
+    else
+      @searches
+    end
   end
 end
